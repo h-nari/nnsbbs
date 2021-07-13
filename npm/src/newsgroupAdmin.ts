1055c1e208cef7d41de7a76c784e8167641c6f4b
Hiroshi Narimatsu <nari@mxb.mesh.ne.jp>
2021-07-13 09:08:26 +0900

    fix: Read/write permission settings are not displayed in stem newsgroups.

--- npm/src/newsgroupAdmin.ts
+++ npm/src/newsgroupAdmin.ts
@@ -178,9 +178,8 @@ export class NewsgroupAdmin {
           !c.hasChild() ?
             form_row('', 3,
               form_check('ng-bLocked', i18next.t('submissions-are-not-allowed'), !postable)) : '',
-          postable ?
-            form_row(i18next.t('access-control'), 3, button({ class: 'form-row btn-permission', type: 'button' },
-              this.permission_html('Read:', 'rpl'), this.permission_html('Write:', 'wpl'))) : '',
+          form_row(i18next.t('access-control'), 3, button({ class: 'form-row btn-permission', type: 'button' },
+            this.permission_html('Read:', 'rpl'), this.permission_html('Write:', 'wpl'))),
           div({ class: 'form-group' },
             label({ class: 'form-label' }, i18next.t('newsgroup-description') as string),
             tag('textarea', { class: 'form-control', id: 'ng-comment', rows: 10 }, c.newsgroup ? c.newsgroup.comment : '')),
